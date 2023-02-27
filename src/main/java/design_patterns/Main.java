6e08a3a538c77232a6fad078ad70373e67916090
bhavani-Zemoso <bhavani.somanchi@zemosolabs.com>
2023-02-27 10:49:44 +0530

    Revert "Factory design pattern for online order processing"
    This reverts commit 94845553ca26b2e6ae7fe69399e1fe7ffd427478.

--- src/main/java/design_patterns/Main.java
+++
@@ -1,7 +0,0 @@
-package design_patterns;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- src/main/java/design_patterns/creational_patterns/factory/Order.java
+++
@@ -1,9 +0,0 @@
-package design_patterns.creational_patterns.factory;
-
-public interface Order {
-
-    void process();
-    void addItem(String item);
-    void deleteItem(String item);
-    void getAllItems();
-}
